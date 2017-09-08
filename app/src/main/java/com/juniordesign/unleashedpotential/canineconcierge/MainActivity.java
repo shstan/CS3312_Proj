c0c3d9a1b4d7cac0ed9b5392f2042b32447baa85
Griffin <niffirg1@gatech.edu>
2017-09-07 23:33:50 -0400

    Added Login and Register and login landing page

--- app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/MainActivity.java
+++ app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/MainActivity.java
@@ -1,27 +1,37 @@
 package com.juniordesign.unleashedpotential.canineconcierge;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogout;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = (Button) findViewById(R.id.sign_out);
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 
-    public void login(View view) {
-        // TODO: attempt login
-        // TODO: print error on message screen if fails
-    }
-
-    public void beginRegistration(View view) {
-        Intent launchRegistration = new Intent(MainActivity.this,RegisterStep1.class);
-        startActivity(launchRegistration);
-    }
 
 }
--- app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/RegisterStep1.java
+++ app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/RegisterStep1.java
@@ -1,17 +1,91 @@
 package com.juniordesign.unleashedpotential.canineconcierge;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterStep1 extends AppCompatActivity {
+    private EditText inputEmail, inputConfirmPassword, inputPassword;
+    private Button btnSignUp;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_step_1);
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignUp = (Button) findViewById(R.id.to_register_step_2);
+        inputEmail = (EditText) findViewById(R.id.email_address);
+        inputPassword = (EditText) findViewById(R.id.password);
+        inputConfirmPassword = (EditText) findViewById(R.id.confirm_password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //Intent to switch to MainActivity
+        Intent mainInt = new Intent(RegisterStep1.this, MainActivity.class);
+        //Intent to switch to LoginActivity
+        final Intent loginInt = new Intent(RegisterStep1.this, LoginActivity.class);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter the password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //start showing progress bar
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterStep1.this, new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // display message if login fails
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(RegisterStep1.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    //otherwise go to login page
+                                    startActivity(loginInt);
+                                    finish();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+
 
     public void goToRegisterStep2(View view) {
         // TODO: validate user input
