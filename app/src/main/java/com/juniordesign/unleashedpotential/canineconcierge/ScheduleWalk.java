fdf8a7d4d9021836eeeb9ea7da79245c6bd66efe
Christy Zachary <christyzachary@gmail.com>
2017-09-13 22:11:29 -0400

    sprint 2 pages skeleton

---
+++ app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/ScheduleWalk.java
@@ -0,0 +1,17 @@
+package com.juniordesign.unleashedpotential.canineconcierge;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chris on 9/13/2017.
+ */
+
+public class ScheduleWalk extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_walk);
+    }
+
+}
