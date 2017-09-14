b69d4836210c08ae2d87d2f1947618c0dcad7b33
Christy Zachary <christyzachary@gmail.com>
2017-09-14 10:54:04 -0400

    calendar and list view on schedule walk page

--- app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/ScheduleWalk.java
+++ app/src/main/java/com/juniordesign/unleashedpotential/canineconcierge/ScheduleWalk.java
@@ -2,6 +2,11 @@ package com.juniordesign.unleashedpotential.canineconcierge;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chris on 9/13/2017.
@@ -9,9 +14,31 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ScheduleWalk extends AppCompatActivity {
 
+    private ListView packLeadersList;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule_walk);
+
+        packLeadersList = (ListView) findViewById(R.id.pack_leaders_list);
+
+        // Instantiating an array list
+        List<String> availablePackLeaders = new ArrayList<String>();
+        availablePackLeaders.add("foo");
+        availablePackLeaders.add("bar");
+        availablePackLeaders.add("hey");
+        availablePackLeaders.add("sup");
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                availablePackLeaders );
+
+        packLeadersList.setAdapter(arrayAdapter);
+
     }
 
 }
